4c849bc455709a606dace836f22348a8ab1545d6
Neel Indap <neel@3playmedia.com>
2019-02-22 13:40:55 -0500

    slash commands for slack

--- server.js
+++ server.js
@@ -10,13 +10,15 @@ const fetch = require("node-fetch");
 var BATHROOMS_COLLECTION = "bathrooms";
 
 var BATHROOMS = [
-  {"5c5dcc98aa6c3e001790f844":
+  {
+    "5c5dcc98aa6c3e001790f844":
     {
       name: "Bathroom%201",
       lastUpdated: null,
     }
   },
-  {"5c5dea87b3ab79e53642272f":
+  {
+    "5c5dea87b3ab79e53642272f":
     {
       name: "Bathroom%202",
       lastUpdated: null,
@@ -82,6 +84,90 @@ app.post("/bathrooms", function (req, res) {
  *    PUT: update bathroom by id
  *    DELETE: deletes bathroom by id
  */
+app.get("/bathrooms/:id", function (req, res) {
+  db.collection(BATHROOMS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to get bathroom");
+    } else {
+      res.status(200).json(doc);
+    }
+  });
+});
+app.put("/bathrooms/:id", function (req, res) {
+  var updateDoc = req.body;
+  delete updateDoc._id;
+  updateSlackChannel(req.params.id, updateDoc.vacant);
+  db.collection(BATHROOMS_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function (err, doc) {
+    if (err) {
+      handleError(res, err.message, "Failed to update bathroom");
+    } else {
+      res.status(204).end();
+    }
+  });
+});
+
+function updateSlackChannel(id, state) {
+  // Get pretty room info
+  let roomName, lastTimestamp, index;
+  for (var i = 0; i < BATHROOMS.length; i++) {
+    room = BATHROOMS[i];
+    if (Object.keys(room)[0] === id) {
+      index = i;
+      roomName = Object.values(room)[0].name;
+      lastTimestamp = Object.values(room)[0].lastUpdated;
+      break;
+    }
+  }
+  console.log("room: " + room);
+  // Delete last message by timestamp
+  if (lastTimestamp) {
+    url = 'https://slack.com/api/chat.delete?token=' + process.env.API_TOKEN + '&channel=GG2E2ETNZ&ts=' + lastTimestamp
+    fetch(url, { method: 'POST' })
+      .then(response => response.json())
+      .then(data => {
+        timestamp = data['ts'];
+        BATHROOMS[index] = {
+          [id]: {
+            name: roomName,
+            lastUpdated: timestamp
+          }
+        }
+      })
+      .catch(error => console.error(error));
+  }
+
+
+  // Post new message
+  stateText = state ? "vacant" : "occupied";
+  postUrl = 'https://slack.com/api/chat.postMessage?token=' + process.env.API_TOKEN + '&channel=bathroom_bot&text=' + roomName + '%20is%20' + stateText
+
+  fetch(postUrl)
+    .then(response => response.json())
+    .then(data => {
+      timestamp = data['message']['ts'];
+      updatedRoom = {
+        [id]: {
+          name: roomName,
+          lastUpdated: timestamp
+        }
+      }
+      BATHROOMS[index] = updatedRoom;
+    })
+    .catch(error => console.error(error))
+}
+
+app.delete("/bathrooms/:id", function (req, res) {
+  db.collection(BATHROOMS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
+    if (err) {
+      handleError(res, err.message, "Failed to delete bathroom");
+    } else {
+      res.status(204).end();
+    }
+  });
+});
+
+
+/* Slack stuff */
 app.post("/slack", function (req, res) {
   var request = req.body;
   var challenge = request.challenge;
@@ -91,19 +177,20 @@ app.post("/slack", function (req, res) {
   var channelType = request.event.channel_type;
   var apiAppId = request.api_app_id;
   var message = "";
-  if("app_mention" === type && text.toLowerCase().indexOf("status") !== -1) {
-    var promise1 = new Promise(function(resolve, reject){
-      db.collection(BATHROOMS_COLLECTION).find().toArray(function(err, result) {
-      result.forEach(function(bathroom){
-        if(bathroom.vacant){
-          message += ":awyeah: :partyparrot: " + bathroom.name + " is vacant! :partyparrot: :awyeah:\n";
-        }else{
-          message += ":awkwardseal: :nicmoji_sad: Uh oh, " + bathroom.name + " is occupied :nicmoji_sad: :awkwardseal:\n";
-        }
-      });
-      resolve(message);
-    })});
-    promise1.then(function(value) {
+  if ("app_mention" === type && text.toLowerCase().indexOf("status") !== -1) {
+    var promise1 = new Promise(function (resolve, reject) {
+      db.collection(BATHROOMS_COLLECTION).find().toArray(function (err, result) {
+        result.forEach(function (bathroom) {
+          if (bathroom.vacant) {
+            message += ":awyeah: :partyparrot: " + bathroom.name + " is vacant! :partyparrot: :awyeah:\n";
+          } else {
+            message += ":awkwardseal: :nicmoji_sad: Uh oh, " + bathroom.name + " is occupied :nicmoji_sad: :awkwardseal:\n";
+          }
+        });
+        resolve(message);
+      })
+    });
+    promise1.then(function (value) {
       var xmlHttp = new XMLHttpRequest();
       theUrl = "https://slack.com/api/chat.postMessage?token=" + process.env.API_TOKEN + "&channel=" + channelName + "&text=" + message + "&pretty=1%20[1]%2013207";
       xmlHttp.open("GET", theUrl, false); // false for synchronous request
@@ -112,7 +199,7 @@ app.post("/slack", function (req, res) {
   }
   res.status(200).send();
 });
-app.post("/slash", function (req, res) {
+app.post("/command", function (req, res) {
   var request = req.body;
   console.log(req);
   var command = req.command;
@@ -147,7 +234,7 @@ app.post("/slash", function (req, res) {
     {
       "response_type": "ephemeral", // they call responses only visible to original users ephemeral
       "text": "You have pinged bathroom bot!",
-       "attachments": attachments
+      "attachments": attachments
     }
 
     var url = request.response_url;
@@ -157,115 +244,4 @@ app.post("/slash", function (req, res) {
     xmlHttp.send(body);
   });
   res.status(200).send();
-});
-app.post("/slash-response", function (req, res) {
-  var attachments = []
-  var promise = new Promise(function (resolve, reject) {
-    db.collection(BATHROOMS_COLLECTION).find().toArray(function (err, result) {
-      result.forEach(function (bathroom) {
-        if (bathroom.vacant) {
-          attachments.push(
-            {
-              "color": "good",
-              "text": `:awyeah: :partyparrot: ${bathroom.name} is vacant! :partyparrot: :awyeah:\n`
-            }
-          )
-        } else {
-          attachments.push(
-            {
-              "color": "danger",
-              "text": `:awkwardseal: :nicmoji_sad: Uh oh, ${bathroom.name} is occupied :nicmoji_sad: :awkwardseal:\n`
-            }
-          )
-        }
-      });
-      resolve(attachments);
-    });
-  });
-  promise.then((attachments) => {
-    response =
-    {
-      "response_type": "ephemeral", // they call responses only visible to original users ephemeral
-      "text": "You have pinged bathroom bot!",
-       "attachments": attachments
-    }
-    res.status(200).json(response);
-  })
-});
-app.get("/bathrooms/:id", function (req, res) {
-  db.collection(BATHROOMS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to get bathroom");
-    } else {
-      res.status(200).json(doc);
-    }
-  });
-});
-app.put("/bathrooms/:id", function (req, res) {
-  var updateDoc = req.body;
-  delete updateDoc._id;
-  updateSlackChannel(req.params.id, updateDoc.vacant);
-  db.collection(BATHROOMS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
-    if (err) {
-      handleError(res, err.message, "Failed to update bathroom");
-    } else {
-      res.status(204).end();
-    }
-  });
-});
-
-function updateSlackChannel(id, state){
-  // Get pretty room info
-  let roomName, lastTimestamp, index;
-  for(var i = 0; i < BATHROOMS.length; i++) {
-    room = BATHROOMS[i];
-    if (Object.keys(room)[0] === id){
-      index = i;
-      roomName = Object.values(room)[0].name;
-      lastTimestamp = Object.values(room)[0].lastUpdated;
-      break;
-    }
-  }
-  console.log("room: " + room);
-  // Delete last message by timestamp
-  if(lastTimestamp){
-    url = 'https://slack.com/api/chat.delete?token=' + process.env.API_TOKEN + '&channel=GG2E2ETNZ&ts=' + lastTimestamp
-    fetch(url, {method: 'POST'})
-      .then(response => response.json())
-      .then(data => {
-        timestamp = data['ts'];
-        BATHROOMS[index] = {[id]:{
-          name: roomName,
-          lastUpdated: timestamp
-        }}
-      })
-      .catch(error => console.error(error));
-  }
-
-
-  // Post new message
-  stateText = state ? "vacant" : "occupied";
-  postUrl = 'https://slack.com/api/chat.postMessage?token=' + process.env.API_TOKEN + '&channel=bathroom_bot&text=' + roomName + '%20is%20' + stateText
-
-  fetch(postUrl)
-    .then(response => response.json())
-    .then(data => {
-      timestamp = data['message']['ts'];
-      updatedRoom = {[id]:{
-        name: roomName,
-        lastUpdated: timestamp
-      }}
-      BATHROOMS[index] = updatedRoom;
-    })
-    .catch(error => console.error(error))
-}
-
-app.delete("/bathrooms/:id", function(req, res) {
-  db.collection(BATHROOMS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
-    if (err) {
-      handleError(res, err.message, "Failed to delete bathroom");
-    } else {
-      res.status(204).end();
-    }
-  });
 });
