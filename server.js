294e3273cd6c1e650945a31d1b2031876aa3d270
Lanya Butler <lanya@3playmedia.com>
2019-02-22 10:54:54 -0600

    slash commands

--- server.js
+++ server.js
@@ -5,49 +5,39 @@ var path = require("path");
 var bodyParser = require("body-parser");
 var mongodb = require("mongodb");
 var ObjectID = mongodb.ObjectID;
-
 var BATHROOMS_COLLECTION = "bathrooms";
-
 var app = express();
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json());
-
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
-
 // Connect to the database before starting the application server.
 mongodb.MongoClient.connect(process.env.MONGODB_URI, function (err, database) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
-
   // Save database object from the callback for reuse.
   db = database;
   console.log("Database connection ready");
-
   // Initialize the app.
   var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
 });
-
 // BATHROOMS API ROUTES BELOW
-
 // Generic error handler used by all endpoints.
 function handleError(res, reason, message, code) {
   console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
+  res.status(code || 500).json({ "error": message });
 }
-
 /*  "/bathrooms"
  *    GET: finds all bathrooms
  *    POST: creates a new bathroom
  */
-
-app.get("/bathrooms", function(req, res) {
-  db.collection(BATHROOMS_COLLECTION).find({}).toArray(function(err, docs) {
+app.get("/bathrooms", function (req, res) {
+  db.collection(BATHROOMS_COLLECTION).find({}).toArray(function (err, docs) {
     if (err) {
       handleError(res, err.message, "Failed to get bathrooms.");
     } else {
@@ -55,16 +45,13 @@ app.get("/bathrooms", function(req, res) {
     }
   });
 });
-
-app.post("/bathrooms", function(req, res) {
+app.post("/bathrooms", function (req, res) {
   var newBathroom = req.body;
   newBathroom.createDate = new Date();
-
   if (!(req.body.name)) {
     handleError(res, "Invalid user input", "Must provide a name.", 400);
   }
-
-  db.collection(BATHROOMS_COLLECTION).insertOne(newBathroom, function(err, doc) {
+  db.collection(BATHROOMS_COLLECTION).insertOne(newBathroom, function (err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to create new bathroom.");
     } else {
@@ -72,14 +59,12 @@ app.post("/bathrooms", function(req, res) {
     }
   });
 });
-
 /*  "/bathrooms/:id"
  *    GET: find bathroom by id
  *    PUT: update bathroom by id
  *    DELETE: deletes bathroom by id
  */
-
-app.post("/slack", function(req, res) {
+app.post("/slack", function (req, res) {
   var request = req.body;
   var challenge = request.challenge;
   var text = request.event.text;
@@ -89,78 +74,117 @@ app.post("/slack", function(req, res) {
   var apiAppId = request.api_app_id;
   var message = "";
   console.log(request);
-  if("app_mention" === type && text.toLowerCase().indexOf("status") !== -1) {
-    var promise1 = new Promise(function(resolve, reject){
-      db.collection(BATHROOMS_COLLECTION).find().toArray(function(err, result) {
-      result.forEach(function(bathroom){
-        if(bathroom.vacant){
-          message += ":awyeah: :partyparrot: " + bathroom.name + " is vacant! :partyparrot: :awyeah:\n";
-        }else{
-          message += ":awkwardseal: :nicmoji_sad: Uh oh, " + bathroom.name + " is occupied :nicmoji_sad: :awkwardseal:\n";
-        }
-      });
-      resolve(message);
-    })});
-    promise1.then(function(value) {
+  if ("app_mention" === type && text.toLowerCase().indexOf("status") !== -1) {
+    var promise1 = new Promise(function (resolve, reject) {
+      db.collection(BATHROOMS_COLLECTION).find().toArray(function (err, result) {
+        result.forEach(function (bathroom) {
+          if (bathroom.vacant) {
+            message += ":awyeah: :partyparrot: " + bathroom.name + " is vacant! :partyparrot: :awyeah:\n";
+          } else {
+            message += ":awkwardseal: :nicmoji_sad: Uh oh, " + bathroom.name + " is occupied :nicmoji_sad: :awkwardseal:\n";
+          }
+        });
+        resolve(message);
+      })
+    });
+    promise1.then(function (value) {
       console.log(value);
       var xmlHttp = new XMLHttpRequest();
       theUrl = "https://slack.com/api/chat.postMessage?token=" + process.env.API_TOKEN + "&channel=" + channelName + "&text=" + message + "&pretty=1%20[1]%2013207";
-      xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-      xmlHttp.send( null );
+      xmlHttp.open("GET", theUrl, false); // false for synchronous request
+      xmlHttp.send(null);
     })
   }
   res.status(200).send();
 });
-
-app.post("/slash", function(req, res) {
+app.post("/slash", function (req, res) {
   var request = req.body;
+  console.log(request);
   var command = req.command;
   var text = request.text;
-
   // respond within 3000ms with status 200 that the request was received
   // in order to respond with pretty text we send a response_url
-
-  response =
-  {
-    "response_type": "ephemeral", // they call responses only visible to original users ephemeral
-    "text": "You have pinged bathroom bot",
-     "response_url": "/slash-response"
-  }
-  res.status(200).json(response);
-
-});
-
-app.get("slash-response", function(req, res) {
-  attachments = []
-
-  db.collection(BATHROOMS_COLLECTION).find().toArray(function(err, result) {
-  result.forEach(function(bathroom){
-    if(bathroom.vacant){
-      attachments.push(
-        [
-          "color": "good",
-          "text": `:awyeah: :partyparrot: #{bathroom.name} is vacant! :partyparrot: :awyeah:\n`
-        ]
-      )
-    } else {
-      attachments.push([
-        "color": "danger",
-        "text": `:awkwardseal: :nicmoji_sad: Uh oh, #{bathroom.name} is occupied :nicmoji_sad: :awkwardseal:\n`
-      ])
+  var attachments = []
+  var promise = new Promise(function (resolve, reject) {
+    db.collection(BATHROOMS_COLLECTION).find().toArray(function (err, result) {
+      result.forEach(function (bathroom) {
+        if (bathroom.vacant) {
+          attachments.push(
+            {
+              "color": "good",
+              "text": `:awyeah: :partyparrot: ${bathroom.name} is vacant! :partyparrot: :awyeah:\n`
+            }
+          )
+        } else {
+          attachments.push(
+            {
+              "color": "danger",
+              "text": `:awkwardseal: :nicmoji_sad: Uh oh, ${bathroom.name} is occupied :nicmoji_sad: :awkwardseal:\n`
+            }
+          )
+        }
+      });
+      resolve(attachments);
+    });
+  });
+  promise.then((attachments) => {
+    var body =
+    {
+      "response_type": "ephemeral", // they call responses only visible to original users ephemeral
+      "text": "You have pinged bathroom bot!",
+       "attachments": attachments
     }
 
-  response =
-  {
-    "response_type": "ephemeral", // they call responses only visible to original users ephemeral
-    "text": "You have pinged bathroom bot!",
-     "attachments": attachments
-  }
-  res.status(200).json(response);
-
+    var url = request.response_url;
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
+    xmlHttp.open("POST", url, true); // false for synchronous request
+    xmlHttp.send(body);
+  })
+  // response =
+  //   {
+  //     "response_type": "ephemeral", // they call responses only visible to original users ephemeral
+  //     "text": "You have pinged bathroom bot",
+  //     "response_url": getResponseForBot
+  //   }
+  // res.status(200).json(response);
 });
-
-app.get("/bathrooms/:id", function(req, res) {
-  db.collection(BATHROOMS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function(err, doc) {
+app.post("/slash-response", function (req, res) {
+  var attachments = []
+  var promise = new Promise(function (resolve, reject) {
+    db.collection(BATHROOMS_COLLECTION).find().toArray(function (err, result) {
+      result.forEach(function (bathroom) {
+        if (bathroom.vacant) {
+          attachments.push(
+            {
+              "color": "good",
+              "text": `:awyeah: :partyparrot: ${bathroom.name} is vacant! :partyparrot: :awyeah:\n`
+            }
+          )
+        } else {
+          attachments.push(
+            {
+              "color": "danger",
+              "text": `:awkwardseal: :nicmoji_sad: Uh oh, ${bathroom.name} is occupied :nicmoji_sad: :awkwardseal:\n`
+            }
+          )
+        }
+      });
+      resolve(attachments);
+    });
+  });
+  promise.then((attachments) => {
+    response =
+    {
+      "response_type": "ephemeral", // they call responses only visible to original users ephemeral
+      "text": "You have pinged bathroom bot!",
+       "attachments": attachments
+    }
+    res.status(200).json(response);
+  })
+});
+app.get("/bathrooms/:id", function (req, res) {
+  db.collection(BATHROOMS_COLLECTION).findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to get bathroom");
     } else {
@@ -168,12 +192,10 @@ app.get("/bathrooms/:id", function(req, res) {
     }
   });
 });
-
-app.put("/bathrooms/:id", function(req, res) {
+app.put("/bathrooms/:id", function (req, res) {
   var updateDoc = req.body;
   delete updateDoc._id;
-
-  db.collection(BATHROOMS_COLLECTION).updateOne({_id: new ObjectID(req.params.id)}, updateDoc, function(err, doc) {
+  db.collection(BATHROOMS_COLLECTION).updateOne({ _id: new ObjectID(req.params.id) }, updateDoc, function (err, doc) {
     if (err) {
       handleError(res, err.message, "Failed to update bathroom");
     } else {
@@ -181,9 +203,8 @@ app.put("/bathrooms/:id", function(req, res) {
     }
   });
 });
-
-app.delete("/bathrooms/:id", function(req, res) {
-  db.collection(BATHROOMS_COLLECTION).deleteOne({_id: new ObjectID(req.params.id)}, function(err, result) {
+app.delete("/bathrooms/:id", function (req, res) {
+  db.collection(BATHROOMS_COLLECTION).deleteOne({ _id: new ObjectID(req.params.id) }, function (err, result) {
     if (err) {
       handleError(res, err.message, "Failed to delete bathroom");
     } else {
